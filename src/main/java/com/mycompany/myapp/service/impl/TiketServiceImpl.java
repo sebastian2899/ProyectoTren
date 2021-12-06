78399bc72bfaa5d90b57d31611ea481e8c2da7bc
sebastian2899 <sebas-1999-7@hotmail.com>
2021-12-06 13:37:42 -0500

    cambios

--- src/main/java/com/mycompany/myapp/service/impl/TiketServiceImpl.java
+++ src/main/java/com/mycompany/myapp/service/impl/TiketServiceImpl.java
@@ -4,6 +4,7 @@ import com.mycompany.myapp.config.EmailUtil;
 import com.mycompany.myapp.domain.Cliente;
 import com.mycompany.myapp.domain.Tiket;
 import com.mycompany.myapp.domain.Tren;
+import com.mycompany.myapp.repository.ClienteRepository;
 import com.mycompany.myapp.repository.TiketRepository;
 import com.mycompany.myapp.repository.TrenRepository;
 import com.mycompany.myapp.service.TiketService;
@@ -12,6 +13,8 @@ import com.mycompany.myapp.service.dto.TiketDTO;
 import com.mycompany.myapp.service.dto.TrenDTO;
 import com.mycompany.myapp.service.mapper.TiketMapper;
 import com.mycompany.myapp.service.mapper.TrenMapper;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -42,16 +45,20 @@ public class TiketServiceImpl implements TiketService {
 
     private final TrenMapper trenMapper;
 
+    private final ClienteRepository clienteRepository;
+
     public TiketServiceImpl(
         TrenMapper trenMapper,
         TiketRepository tiketRepository,
         TiketMapper tiketMapper,
-        TrenRepository trenRepository
+        TrenRepository trenRepository,
+        ClienteRepository clienteRepository
     ) {
         this.tiketRepository = tiketRepository;
         this.tiketMapper = tiketMapper;
         this.trenMapper = trenMapper;
         this.trenRepository = trenRepository;
+        this.clienteRepository = clienteRepository;
     }
 
     @Override
@@ -59,6 +66,23 @@ public class TiketServiceImpl implements TiketService {
         log.debug("Request to save Tiket : {}", tiketDTO);
         Tiket tiket = tiketMapper.toEntity(tiketDTO);
         tiket.setNombreCliente(nombreCliente(tiket.getClienteId()));
+
+        Cliente cliente = clienteRepository.buscarCliente(tiketDTO.getClienteId());
+        BigDecimal precioTotal = tiket.getPrecioTotal();
+
+        BigDecimal descPremiun = new BigDecimal(0.10);
+        BigDecimal descPremiunPlus = new BigDecimal(0.2);
+
+        if (cliente.getTipoCliente().equals("Premium")) {
+            BigDecimal decs = tiket.getPrecioTotal().multiply(descPremiun);
+            tiket.setPrecioTotal(precioTotal.subtract(decs));
+        }
+
+        if (cliente.getTipoCliente().equals("Premium Plus")) {
+            BigDecimal desc = tiket.getPrecioTotal().multiply(descPremiunPlus);
+            tiket.setPrecioTotal(precioTotal.subtract(desc));
+        }
+
         tiket = tiketRepository.save(tiket);
         enviarCorreoConfirmacion(tiket);
         return tiketMapper.toDto(tiket);
@@ -66,6 +90,11 @@ public class TiketServiceImpl implements TiketService {
 
     private void enviarCorreoConfirmacion(Tiket tiket) {
         Cliente cliente = tiketRepository.buscarCliente(tiket.getClienteId());
+
+        DecimalFormat df = new DecimalFormat();
+
+        df.setMaximumFractionDigits(6);
+        String format = df.format(tiket.getPrecioTotal());
 
         if (tiket.getEstado().equals("Activo") && cliente.getCorreo() != null && !cliente.getCorreo().isEmpty()) {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MMMMM.dd hh:mm aaa");
@@ -77,7 +106,7 @@ public class TiketServiceImpl implements TiketService {
                 "--- Jornada: " +
                 tiket.getJordana() +
                 "--- Precio del tiket: " +
-                tiket.getPrecioTotal();
+                format;
             try {
                 log.debug("Enviando correo de confirmacion a: " + cliente.getCorreo());
                 EmailUtil.sendArchivoTLS(null, cliente.getCorreo().trim(), "Confirmación Tiket", mensaje);
